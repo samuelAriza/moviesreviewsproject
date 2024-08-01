dcb81578ba2e6b24788b29cd79787a4ec138432b
samargo <samargo0317@gmail.com>
2024-08-01 11:34:52 -0500

    busqueda de películas

--- moviesreviews/settings.py
+++ moviesreviews/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'moviesreviews.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR + "/" + "db.sqlite3",
     }
 }
 
@@ -120,8 +120,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / 'movie/static',
+    BASE_DIR + "/" + 'movie/static',
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR + "/" + "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
