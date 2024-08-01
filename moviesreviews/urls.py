299a5ec44690b3cafbe11dd20f1c33bcb4490086
samargo <samargo0317@gmail.com>
2024-08-01 10:59:34 -0500

    cambiando el color del nombre

--- moviesreviews/urls.py
+++ moviesreviews/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from movie import views as movieViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home),
     path('about/', movieViews.about),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
